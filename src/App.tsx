99043d7a2cae43447adffbc327430731f33a0cb8
Faisal <faisalimtiaz@logicon.com.pk>
2023-03-23 17:34:21 +0500

    under construction added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import CodeIcon from "@mui/icons-material/Code";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>My portfolio will be added here soon</h1>
+      <div>
+        <CodeIcon style={{ fontSize: "260px" }} />
+      </div>
+      <div>
+        <h2>Sorry for inconvenience, site is under construction</h2>
+        <h4>Meanwhile, you can use alternative route - Thanks</h4>
+        <a href="https://www.linkedin.com/in/faisal-imtiaz" target="_blank">
+          <span style={{ cursor: "pointer", padding: "5px" }} ><LinkedInIcon /></span>
+          </a>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
