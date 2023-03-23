0226632ba7a2508fb01287eea824ef38c21b3263
Faisal <faisalimtiaz@logicon.com.pk>
2023-03-23 16:51:53 +0500

    added soon

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My portfolio will be added here</h1>
+      <h1>My portfolio will be added here soon</h1>
     </div>
   );
 }
