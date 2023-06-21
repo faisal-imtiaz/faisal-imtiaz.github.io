205519c58db4cc04408dc4b586a7489e365f646b
Faisal Imtiaz <faisalimtiaz092@gmail.com>
2023-06-21 18:37:03 +0500

    added actively

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <CodeIcon style={{ fontSize: "260px" }} />
       </div>
       <div>
-        <h2>Sorry for inconvenience, site is under construction</h2>
+        <h2>Sorry for inconvenience, site is under construction actively</h2>
         <h4>Meanwhile, you can use alternative route - Thanks</h4>
         <a href="https://www.linkedin.com/in/faisal-imtiaz" target="_blank">
           <span style={{ cursor: "pointer", padding: "5px" }} ><LinkedInIcon /></span>
